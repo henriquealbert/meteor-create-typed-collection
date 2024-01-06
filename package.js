126cf19de16628588d6e593cc8568b79b080538e
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2024-01-06 13:39:00 -0300

    update to 0.0.2

--- package.js
+++ package.js
@@ -1,19 +1,19 @@
 Package.describe({
-  git: "https://github.com/henriquealbert/meteor-create-typed-collection",
-  name: "hschmaiske:create-typed-collection",
-  version: "0.0.1",
-  summary: "Create typed safe collections for Meteor",
-  documentation: "README.md",
+    git: "https://github.com/henriquealbert/meteor-create-typed-collection",
+    name: "hschmaiske:create-typed-collection",
+    version: "0.0.2",
+    summary: "Create typed safe collections for Meteor",
+    documentation: "README.md",
 });
 
 Npm.depends({
-  zod: "3.22.4",
+    zod: "3.22.4",
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom("2.13.3");
-  api.use("ecmascript");
-  api.use("typescript");
-  api.use("zodern:types@1.0.0");
-  api.mainModule("src/index.ts");
+    api.versionsFrom("2.13.3", "3.0-beta.0");
+    api.use("ecmascript");
+    api.use("typescript");
+    api.use("zodern:types@1.0.0");
+    api.mainModule("src/index.ts");
 });
