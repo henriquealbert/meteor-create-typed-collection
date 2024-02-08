58b6f380bdb5dfbcb19c3710627e89726fc8f49a
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2024-02-08 11:30:36 -0300

    Add createRemoteTypedCollection function

---
+++ src/create-remote-typed-collection.ts
@@ -0,0 +1,27 @@
+import { newRemoteCollectionDriver } from "./new-remote-collection-driver";
+import { ZodRawShape, z } from "zod";
+import { createRemoteCollection } from "./create-remote-collection";
+import { CreateRemoteTypedCollection } from "./types";
+
+export const createRemoteTypedCollection = <
+    T extends ZodRawShape,
+    U extends Record<string, (...args: any[]) => any>
+>({
+    mongoUrl,
+    schema,
+    stackName,
+    customCollectionMethods,
+    name,
+}: CreateRemoteTypedCollection<T, U>) => {
+    const _driver = newRemoteCollectionDriver({ mongoUrl });
+
+    const collection = createRemoteCollection({
+        name,
+        schema,
+        customCollectionMethods,
+        _driver,
+        stackName,
+    });
+
+    return collection;
+};
